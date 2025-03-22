b6ddb6f6a1de15fcf71a5fb9c24ed754b1852ef2
sabonawebsite-com <sabonamarara53@gmail.com>
2025-03-22 14:27:07 +0300

    first commit

--- index.js
+++ index.js
@@ -38,3 +38,18 @@ fetch("data.json")
     });
 })
 .catch((error) => console.error("Error loading data:", error));
+
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+  document.addEventListener('copy', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('cut', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('paste', function (e) {
+  e.preventDefault();
+});
